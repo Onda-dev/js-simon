3aeb1050db8914b2e8f8d97384728c1bb36985dc
Onda-dev <manuelaltieri3@gmail.com>
2022-05-12 14:29:14 +0200

    Parte 3 ok

--- main.js
+++ main.js
@@ -4,16 +4,27 @@
 
 // 1. Creare 5 numeri random
 const randomNumbers = []
-
-for (let i = 0; i < 5; i++) {
-    number = Math.floor(Math.random() * 10) + 1;
-    while (!randomNumbers.includes(number)) {
-        randomNumbers.push(number)
-    }
+let number
+    
+while (!randomNumbers.includes(number) || randomNumbers.length < 5) {
+    number = Math.floor(Math.random() * 10) + 1
+    randomNumbers.push(number)
 }
-
+alert(randomNumbers)
 console.log(randomNumbers)
-// 2. Far partire un countdown da 30 a 0
-
+// 2. Far partire un countdown di 30 secondi
+setTimeout(function(){
+    challenge()
+    console.log("timer")
+}, 3000)
 // 3. Allo scadere del tempo, creare 5 prompt in cui l'utente deve inserire i 5 numeri, uno per volta
+const attempts = []
+let attempt
+function challenge() {
+    for (let i = 0; i < 5; i++){
+        attempt = Number(prompt("Inserisci, uno per volta, i numeri mostrati in precedenza"))
+        attempts.push(attempt)
+    }
+    console.log(attempts)
+}
 // 4. Creare alert con numero di numeri indovinati e quali
