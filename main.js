b044a5d7da92bd308e6e31c6f7ba8c15674d9273
Onda-dev <manuelaltieri3@gmail.com>
2022-05-12 15:17:04 +0200

    Corretta posizione variabili

--- main.js
+++ main.js
@@ -3,10 +3,11 @@
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 // 1. Creare 5 numeri random
+
 const randomNumbers = []
-let number
     
 while (randomNumbers.length < 5) {
+    let number
     number = Math.floor(Math.random() * 10) + 1
     if (!randomNumbers.includes(number)) {
         randomNumbers.push(number)
@@ -20,11 +21,10 @@ setTimeout(function(){
 }, 3000)
 // 3. Allo scadere del tempo, creare 5 prompt in cui l'utente deve inserire i 5 numeri, uno per volta
 const attempts = []
-let attempt
 const guessed = []
 
 function challenge() {
-
+    let attempt
     while (isNaN(attempt) || attempts.length < 5) {
         attempt = Number(prompt("Inserisci, uno per volta, i numeri mostrati in precedenza"))
         attempts.push(attempt)
